f78d48ca4919dfc2383a711f0d63fe5f356aae0a
Zoltan Kovago <zoltan.kovago@hu.ibm.com>
2025-05-06 17:39:33 +0200

    refactor: Put the infinite loop into a task

--- main/main.c
+++ main/main.c
@@ -5,18 +5,33 @@
 #include "midi/midi.h"
 #include "midi/notes.h"
 
-#define MIDI_CH 5
+void midi_task(void *pvParameters)
+{
+    uint8_t channel  = 5;
+    uint8_t note     = NOTE_C4;
+    uint8_t velocity = 100;
 
-void app_main(void) {
-    midi_init(UART_NUM_2, GPIO_NUM_18);
+    while (true) {
+        midi_note_on(channel, note, velocity);
+        vTaskDelay(pdMS_TO_TICKS(300));
 
-    while (1) {
-        midi_note_on(MIDI_CH, NOTE_C4, 100);
-        vTaskDelay(pdMS_TO_TICKS(200));
-
-        midi_note_off(MIDI_CH, NOTE_C4, 64);
+        midi_note_off(channel, note, 64);
         vTaskDelay(pdMS_TO_TICKS(1000));
 
         ESP_LOGI("main", "Note sent");
     }
+}
+
+void app_main(void)
+{
+    midi_init(UART_NUM_2, GPIO_NUM_18);
+
+    xTaskCreate(
+        midi_task,         // Task function
+        "midi_task",       // Task name (for debugging)
+        2048,              // Stack size in words (not bytes)
+        NULL,              // Task parameter
+        5,                 // Priority (higher number = higher priority)
+        NULL               // Task handle (not used here)
+    );
 }
