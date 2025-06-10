c492d295ac669a054a80f09937bbdd3fe6a67bbd
Zoltan Kovago <zoltan.kovago@hu.ibm.com>
2025-06-10 11:20:08 +0200

    fix(transmitter): Do not block the event loop

--- components/transmitter/transmitter.c
+++ components/transmitter/transmitter.c
@@ -7,9 +7,9 @@
 
 #define TRANSMITTER_CHANNEL 5
 
-static void on_note_event(void *arg, esp_event_base_t base, int32_t id, void *data)
+static void transmitter_send_note_task(void *pvParameters)
 {
-    sequencer_step_trig_event_t trig = *(sequencer_step_trig_event_t*) data;
+    sequencer_step_trig_event_t trig = *(sequencer_step_trig_event_t*) pvParameters;
 
     // Note ON
     midi_out_note_on(TRANSMITTER_CHANNEL, trig.note, MIDI_NOTE_VELOCITY_MAX);
@@ -19,6 +19,15 @@ static void on_note_event(void *arg, esp_event_base_t base, int32_t id, void *da
 
     // Note OFF
     midi_out_note_off(TRANSMITTER_CHANNEL, trig.note, 0);
+
+    vTaskDelete(NULL);
+}
+
+static void transmitter_on_note_event(void *arg, esp_event_base_t base, int32_t id, void *data)
+{
+    // Creating a new task is the way to not block the event loop
+    // with calling `vTaskDelay` (between note on and off).
+    xTaskCreate(transmitter_send_note_task, NULL, 2048, data, 5, NULL);
 }
 
 void transmitter_init(esp_event_loop_handle_t event_loop)
@@ -32,7 +41,7 @@ void transmitter_init(esp_event_loop_handle_t event_loop)
         event_loop,
         SEQUENCER_EVENT,
         SEQUENCER_EVENT_STEP_TRIG,
-        on_note_event,
+        transmitter_on_note_event,
         NULL
     );
 }
